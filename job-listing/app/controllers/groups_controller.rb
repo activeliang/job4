e30c050826f7599ba04c8fda21b3b603085d8785
wenweishen <swwsww203@126.com>
2017-05-03 14:10:10 +0800

    move form to partial

--- job-listing/app/controllers/groups_controller.rb
+++ job-listing/app/controllers/groups_controller.rb
@@ -10,6 +10,7 @@ class GroupsController < ApplicationController
   # GET /groups/1
   # GET /groups/1.json
   def show
+    @group = Group.find(params[:id])
   end
 
   # GET /groups/new
@@ -19,47 +20,40 @@ class GroupsController < ApplicationController
 
   # GET /groups/1/edit
   def edit
+    @group = Group.find(params[:id])
   end
 
   # POST /groups
   # POST /groups.json
   def create
     @group = Group.new(group_params)
-
-    respond_to do |format|
-      if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
-        format.json { render :show, status: :created, location: @group }
-      else
-        format.html { render :new }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    @group.save
+    if @group.save
+     redirect_to groups_path
+    else
+     render :new
     end
   end
 
   # PATCH/PUT /groups/1
   # PATCH/PUT /groups/1.json
   def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+    redirect_to groups_path, notice: "Update Success"
+    else
+      render :edit
     end
   end
 
   # DELETE /groups/1
   # DELETE /groups/1.json
   def destroy
+    @group = Group.find(params[:id])
     @group.destroy
-    respond_to do |format|
-      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to groups_path, notice: "Update Success"
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -71,4 +65,4 @@ class GroupsController < ApplicationController
     def group_params
       params.require(:group).permit(:title, :description)
     end
-end
+  end
